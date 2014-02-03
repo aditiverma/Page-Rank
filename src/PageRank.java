1330f2a276c2f9dd4adae801bc9d1a67a2643e02
Aditi <aditi.verma34@gmail.com>
2014-02-02 22:14:11 -0500

    adding ranks

--- src/PageRank.java
+++ src/PageRank.java
@@ -20,106 +20,89 @@ import org.apache.hadoop.util.*;
 import org.jdom.*;
 import org.jdom.input.*;
 import org.jdom.JDOMException;
-	
-	public class PageRank {
-		public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {  
-	//	private final static IntWritable one = new IntWritable(1);
-	      private Text word = new Text();
-	      public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-	    	  String xmlString = value.toString();
-	    	  SAXBuilder builder = new SAXBuilder();
-	            Reader in = new StringReader(xmlString);
-	            String value1="";
-	            Document doc;
-				try {
+
+public class PageRank {
+	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {  
+		private Text word = new Text();
+		public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+			String xmlString = value.toString();
+			SAXBuilder builder = new SAXBuilder();
+			Reader in = new StringReader(xmlString);
+			String value1="";
+			Document doc;
+			try {
 				doc = builder.build(in);
 				Element root = doc.getRootElement();
 				String title=""; String allText="";
 				if(root.getChild("title")!=null)
 				{
-					 title =root.getChildText("title");//later replace spaces by _ //changed from getchild.gettext to this function 
-					 title = title.replaceAll(" ", "_");
+					title =root.getChildText("title");//later replace spaces by _ //changed from getchild.gettext to this function 
+					title = title.replaceAll(" ", "_");
 				}
 				else
 				{
-					 title="no title";
+					title="no title";
 				}
 				if(root.getChild("revision")!=null)
 				{
-	             allText =root.getChild("revision").getChildText("text");//this is actually inside <revision>
-	             allText=allText.replaceAll(" ", "_");//must assign here
+					allText =root.getChild("revision").getChildText("text");//this is actually inside <revision>
+					allText=allText.replaceAll(" ", "_");//must assign here
 				}
 				else
 				{
-				 allText="no text [[notext]] ";
+					allText="no text [[notext]] ";
 				}
-	            //Pattern p1 = Pattern.compile("^.*(\\s\\[\\[[a-zA-Z0-9_ ]+\\]\\]\\s).*$");
-				//Pattern p1 = Pattern.compile("^\\[\\[[a-zA-Z0-9_ ]+\\]\\]$");
-				//Pattern p1 = Pattern.compile("\\[\\[(.*?)\\]\\]");
 				Pattern p1 = Pattern.compile("\\[\\[([^\\]^\\[]+)\\]\\]");
-	    //        Pattern p2 = Pattern.compile("^\\s\\[\\[[a-zA-Z0-9_ \\|]+\\]\\]\\s$");//PAttern matched twice!
-	            Matcher m1 = p1.matcher(allText);
-	          //  Matcher m2 = p2.matcher(allText);
-	            while(m1.find())
-	             {
-	            String textValue=m1.group(1);
-	            if(textValue.contains("|"))
-	            {
-	            	textValue= textValue.substring(0, textValue.indexOf('|'));
-	            }
-	            output.collect(new Text(title), new Text(textValue));
-	          	//output.collect(new Text(title), new Text(allText));
-	             }
-//	            while(m2.find())
-//	             {
-//	            	output.collect(new Text(title), new Text(m2.group()));
-//	            	//output.collect(new Text("abc2 title"), new Text("abc2 text") );
-//	             }
-				} catch (JDOMException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				catch(NullPointerException e)
+				Matcher m1 = p1.matcher(allText);
+				while(m1.find())
 				{
-					e.printStackTrace();	
+					String textValue=m1.group(1);
+					if(textValue.contains("|"))
+					{
+						textValue= textValue.substring(0, textValue.indexOf('|'));
+					}
+					output.collect(new Text(title), new Text(textValue));
 				}
-	      }
-	        
-	    }
-	
-	    public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
-	    	public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-	    	StringBuilder result = new StringBuilder();				//Strings''
-	        while (values.hasNext()) {
-	        	result.append(new Text(values.next()));
-	        	result.append("\t");
-	        }
-	       // output.collect(new Text("abc3 title"), new Text("abc3 text") );
-	        output.collect(key, new Text(new String(result)));
-	      }
-	    }
-	
-	    public static void main(String[] args) throws Exception {
-	      JobConf conf = new JobConf(PageRank.class);
-	      conf.setJobName("PageRank");//job name
-	
-	      conf.setInputFormat(XmlInputFormat.class); 
-	      conf.set("xmlinput.start", "<page>");
-	      conf.set("xmlinput.end", "</page>");
-	      
-	      conf.setOutputKeyClass(Text.class);
-	      conf.setOutputValueClass(Text.class);
-	      
-	      conf.setMapperClass(Map.class);
-	   //  conf.setCombinerClass(Reduce.class);
-	      conf.setReducerClass(Reduce.class);
-	      
-	      conf.setOutputFormat(TextOutputFormat.class);
-	    //  System.out.println("no. of map tasks"+conf.getNumMapTasks());-different from no. of times map() is called
-	      FileInputFormat.setInputPaths(conf, new Path(args[0]));
-	      FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-	
-	      JobClient.runJob(conf);
-	    }
+			} catch (JDOMException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch(NullPointerException e)
+			{
+				e.printStackTrace();	
+			}
+		}
+
 	}
-	
+
+	public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+		public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+			StringBuilder result = new StringBuilder();				//Strings''
+			while (values.hasNext()) {
+				result.append(new Text(values.next()));
+				result.append("\t");
+			}
+			output.collect(key, new Text(new String(result)));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		JobConf conf = new JobConf(PageRank.class);
+		conf.setJobName("PageRank");//job name
+
+		conf.setInputFormat(XmlInputFormat.class); 
+		conf.set("xmlinput.start", "<page>");
+		conf.set("xmlinput.end", "</page>");
+
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(Text.class);
+
+		conf.setMapperClass(Map.class);
+		conf.setReducerClass(Reduce.class);
+
+		conf.setOutputFormat(TextOutputFormat.class);
+		FileInputFormat.setInputPaths(conf, new Path(args[0]));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+		JobClient.runJob(conf);
+	}
+}
--- src/PageRankSorted.java
+++ src/PageRankSorted.java
@@ -22,60 +22,43 @@ import org.apache.hadoop.util.*;
 import org.jdom.*;
 import org.jdom.input.*;
 import org.jdom.JDOMException;
-	
-	public class PageRankSorted{
-		public static int N;
-		public static class MapSorted extends MapReduceBase implements Mapper<LongWritable, Text, Double, Text> {  
-			private Text word = new Text();
-	      public void map(LongWritable key, Text value, OutputCollector<Double, Text> output, Reporter reporter) throws IOException {
-	    	  //word.set("a");  
-	    	  String []values = value.toString().split("\t");
-	    	//  SortedRanks s = new SortedRanks();
-	        Double val = Double.valueOf(values[1]);
-	        N=215549;//remove the hardcoding
-	        if(val>5/N)
-	        {
-	    	  output.collect(val, new Text(values[0]));
-	        }
-	    	  //output.collect(new Text(values[1]), new Text(values[0]));
-		
-	      }
-	        
-	    }
-		
-	
-	    public static class ReduceSorted extends MapReduceBase implements Reducer<Double, Text, Double, Text> {
-	    	public void reduce(Double key, Iterator<Text> values, OutputCollector<Double, Text> output, Reporter reporter) throws IOException {
-	    	//	int result=0;
-	    	//	while(values.hasNext())
-	    	//	{
-	    	Text value= values.next();
-	         output.collect(key, value);
-	    	//	output.collect(new Text(key), value);
-	    	//	}
-	        //should key val be interchanged?
-	      }
-	    }	
-	    public static void main(String[] args) throws Exception {
-	      JobConf conf = new JobConf(PageRankSorted.class);
-	      conf.setJobName("PageRankSorted");//job name
-	      conf.setInputFormat(TextInputFormat.class); 
-	       conf.setOutputKeyClass(Double.class); //map
-	      conf.setOutputValueClass(Text.class); //map
-	     
-	      conf.setOutputKeyComparatorClass(LongWritable.DecreasingComparator.class);
-	      //setOutputValueGroupingComparator(Class)
-	      
-	      conf.setMapperClass(MapSorted.class);
-	   //  conf.setCombinerClass(Reduce.class);
-	      conf.setReducerClass(ReduceSorted.class);
-	    // conf.setNumReduceTasks(1);
-	      conf.setOutputFormat(TextOutputFormat.class);
-	    //  System.out.println("no. of map tasks"+conf.getNumMapTasks());-different from no. of times map() is called
-	      FileInputFormat.setInputPaths(conf, new Path(args[0]));
-	      FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-	
-	      JobClient.runJob(conf);
-	    }
+
+public class PageRankSorted{
+	public static int N;
+	public static class MapSorted extends MapReduceBase implements Mapper<LongWritable, Text, Double, Text> {  
+		private Text word = new Text();
+		public void map(LongWritable key, Text value, OutputCollector<Double, Text> output, Reporter reporter) throws IOException {
+			String []values = value.toString().split("\t");
+			Double val = Double.valueOf(values[1]);
+			N=215549;//remove the hardcoding
+			if(val>5/N)
+			{
+				output.collect(val, new Text(values[0]));
+			}
+		}
+
 	}
-	
+
+
+	public static class ReduceSorted extends MapReduceBase implements Reducer<Double, Text, Double, Text> {
+		public void reduce(Double key, Iterator<Text> values, OutputCollector<Double, Text> output, Reporter reporter) throws IOException {
+			Text value= values.next();
+			output.collect(key, value);
+		}
+	}	
+	public static void main(String[] args) throws Exception {
+		JobConf conf = new JobConf(PageRankSorted.class);
+		conf.setJobName("PageRankSorted");//job name
+		conf.setInputFormat(TextInputFormat.class); 
+		conf.setOutputKeyClass(Double.class); //map
+		conf.setOutputValueClass(Text.class); //map
+		conf.setOutputKeyComparatorClass(LongWritable.DecreasingComparator.class);
+		conf.setMapperClass(MapSorted.class);
+		conf.setReducerClass(ReduceSorted.class);
+		conf.setOutputFormat(TextOutputFormat.class);
+		FileInputFormat.setInputPaths(conf, new Path(args[0]));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+		JobClient.runJob(conf);
+	}
+}
